649f3e2196329e4449ab20f438679f34c4857764
wiryadev <abrarwiryawan@gmail.com>
2023-03-02 21:20:34 +0700

    add layout

---
+++ ImageDownloader/MovieTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieTableViewCell.swift
+//  ImageDownloader
+//
+//  Created by Abrar Wiryawan on 01/03/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var indicatorLoading: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ImageDownloader/ViewController.swift
+++ ImageDownloader/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        movieTableView.dataSource = self
+        movieTableView.register(
+            UINib(nibName: "MovieTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "movieTableViewCell"
+        )
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return movies.count
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+            withIdentifier: "movieTableViewCell",
+            for: indexPath
+        ) as? MovieTableViewCell {
+            let movie = movies[indexPath.row]
+            cell.movieTitle.text = movie.title
+            
+            cell.indicatorLoading.startAnimating()
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+}
